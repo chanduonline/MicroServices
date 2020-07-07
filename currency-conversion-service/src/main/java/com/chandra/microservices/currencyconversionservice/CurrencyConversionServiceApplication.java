b93ca6b27a9d521c335c1d8ae140fba050718912
Chandu <67627909+chanduonline@users.noreply.github.com>
2020-07-07 13:57:04 +0530

    Add files via upload
    Micro Service Load Balancing and Eliminate Hard-Coded URL using Ribbon

---
+++ currency-conversion-service/src/main/java/com/chandra/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.chandra.microservices.currencyconversionservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@SpringBootApplication
+@EnableFeignClients("com.chandra.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication 
+{
+public static void main(String[] args) 
+{
+SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+}
+}
